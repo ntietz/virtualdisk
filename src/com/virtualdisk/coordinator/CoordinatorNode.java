e855f71b0849fedd5d47fdbc25e28421951cd2ea
Nicholas Tietz <ntietz@gmail.com>
2012-06-20 16:37:52 -0400

    Began configuration implementation so server can run soon.

---
+++ src/com/virtualdisk/coordinator/CoordinatorNode.java
@@ -0,0 +1,39 @@
+package com.virtualdisk.coordinator;
+
+public class CoordinatorNode
+{
+    private static Coordinator coordinator;
+    private static CoordinatorServer server;
+    private static CoordinatorNode node;
+
+    private CoordinatorNode()
+    {
+        // if coordinator is null, initialize it
+
+        // if server is null, initialize it
+    }
+
+    public static Coordinator getCoordinator()
+    {
+        return coordinator;
+    }
+
+    public static CoordinatorServer getServer()
+    {
+        return server;
+    }
+
+    public static CoordinatorNode getNode()
+    {
+        if (node != null)
+        {
+            return node;
+        }
+        else
+        {
+            this = new CoordinatorNode();
+            return node;
+        }
+    }
+}
+
