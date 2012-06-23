71d4060b5622790e2c458a7153ff73883eb6ec82
Nicholas Tietz <ntietz@gmail.com>
2012-06-23 15:15:33 -0400

    Began datanode handler code

--- src/com/virtualdisk/network/DataNodeHandler.java
+++ src/com/virtualdisk/network/DataNodeHandler.java
@@ -18,21 +18,33 @@ extends SimpleChannelHandler
         switch (type)
         {
             case orderRequest:
+                // TODO
+                // perform the order request
+                // return the result on the same channel it came in on
                 break;
 
             case readRequest:
+                // TODO
+                // perform the read request
+                // return the result
                 break;
 
             case writeRequest:
+                // TODO
+                // perform the write request
+                // return the result
                 break;
 
             case volumeExistsRequest:
+                // TODO
                 break;
 
             case createVolumeRequest:
+                // TODO
                 break;
 
             case deleteVolumeRequest:
+                // TODO
                 break;
 
             default:
