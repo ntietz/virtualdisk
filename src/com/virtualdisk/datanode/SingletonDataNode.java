dc04b615f2935986bc588f350ebac103f4605dc0
Nicholas Tietz <ntietz@gmail.com>
2012-06-23 22:07:10 -0400

    Added order, read handlers.

--- src/com/virtualdisk/datanode/SingletonDataNode.java
+++ src/com/virtualdisk/datanode/SingletonDataNode.java
@@ -3,5 +3,17 @@ package com.virtualdisk.datanode;
 public class SingletonDataNode
 {
     private static DataNode dataNode;
+    private static SingletonDataNode singleton;
+
+    private SingletonDataNode()
+    {
+        // ...
+    }
+
+    public static DataNode getDataNode()
+    {
+        return dataNode;
+    }
+
 }
 
--- src/com/virtualdisk/network/DataNodeHandler.java
+++ src/com/virtualdisk/network/DataNodeHandler.java
@@ -1,33 +1,67 @@
 package com.virtualdisk.network;
 
+import com.virtualdisk.datanode.*;
+import com.virtualdisk.network.request.*;
 import com.virtualdisk.network.util.*;
 import com.virtualdisk.network.util.Sendable.MessageType;
 
 import org.jboss.netty.channel.*;
 
+import java.util.*;
+
 public class DataNodeHandler
 extends SimpleChannelHandler
 {
+    DataNode dataNode = SingletonDataNode.getDataNode();
+
     public void messageReceived( ChannelHandlerContext context
                                , MessageEvent event
                                )
     {
-        Sendable request = (Sendable) event.getMessage();
-        MessageType type = request.messageType();
+        Sendable rawRequest = (Sendable) event.getMessage();
+        MessageType type = rawRequest.messageType();
 
         switch (type)
         {
-            case orderRequest:
-                // TODO
-                // perform the order request
-                // return the result on the same channel it came in on
-                break;
+            case orderRequest: {
+                OrderRequest request = (OrderRequest) rawRequest;
+                int volumeId = request.getVolumeId();
+                long logicalOffset = request.getLogicalOffset();
+                Date timestamp = request.getTimestamp();
 
-            case readRequest:
-                // TODO
-                // perform the read request
-                // return the result
-                break;
+                int requestId = request.getRequestId();
+                boolean done = true;
+                boolean success = dataNode.order(volumeId, logicalOffset, timestamp);
+
+                Channel coordinatorChannel = event.getChannel();
+                OrderRequestResult result = new OrderRequestResult(requestId, done, success);
+                coordinatorChannel.write(result);
+                } break;
+
+            case readRequest: {
+                ReadRequest request = (ReadRequest) rawRequest;
+                int volumeId = request.getVolumeId();
+                long logicalOffset = request.getLogicalOffset();
+
+                int requestId = request.getRequestId();
+                boolean done = true;
+                byte[] block = dataNode.read(volumeId, logicalOffset);
+                boolean success;
+                if (block == null)
+                {
+                    success = false;
+                    block = new byte[0];
+                }
+                else
+                {
+                    success = true;
+                }
+                Date timestamp = dataNode.getValueTimestamp(volumeId, logicalOffset);
+
+                Channel coordinatorChannel = event.getChannel();
+                ReadRequestResult result = new ReadRequestResult(requestId, done, success, timestamp, block);
+                coordinatorChannel.write(result);
+                } break;
 
             case writeRequest:
                 // TODO
