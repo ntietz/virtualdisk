3624f8aa743b78b2ff346a3c55cdd19e393d34b8
Nicholas Tietz <ntietz@gmail.com>
2012-06-24 14:29:12 -0400

    Added blank file for client program.

---
+++ src/com/virtualdisk/main/ClientMain.java
@@ -0,0 +1,13 @@
+package com.virtualdisk.main;
+
+public class ClientMain
+{
+    public static void main(String... args)
+    {
+        String coordinatorHost = args[0];
+        int coordinatorPort = Integer.valueOf(args[1]);
+
+        
+    }
+}
+
