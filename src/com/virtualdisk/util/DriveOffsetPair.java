cbc7979f71077f350a71600ce773fd7300fedb43
Nicholas Tietz <ntietz@gmail.com>
2012-06-27 11:37:22 -0400

    Added javadoc comments to the com.virtualdisk.util.* classes

--- src/com/virtualdisk/util/DriveOffsetPair.java
+++ src/com/virtualdisk/util/DriveOffsetPair.java
@@ -1,21 +1,45 @@
 package com.virtualdisk.util;
 
+/**
+ * DriveOffsetPair is a convenience class, primarily for use in the DataNode class.
+ * It is used for addressing physical locations are (drive, location) pairs.
+ * 
+ * @author  Nicholas Tietz
+ */
 public class DriveOffsetPair
 {
+    /**
+     * The index within a list of drives to which this pair points.
+     */
     protected int driveNumber;
+
+    /**
+     * The physical offset we are looking for.
+     */
     protected long offset;
 
-    public DriveOffsetPair(int d, long o)
+    /**
+     * Standard constructor.
+     * @param   driveNumber the drive we wish to point to, as an index within a list
+     * @param   offset      the location we wish to point to on the disk
+     */
+    public DriveOffsetPair(int driveNumber, long offset)
     {
-        driveNumber = d;
-        offset = o;
+        this.driveNumber = driveNumber;
+        this.offset = offset;
     }
 
+    /**
+     * @return  the drive number of the pair
+     */
     public int getDriveNumber()
     {
         return driveNumber;
     }
 
+    /**
+     * @return  the offset of the pair
+     */
     public long getOffset()
     {
         return offset;
--- src/com/virtualdisk/util/Range.java
+++ src/com/virtualdisk/util/Range.java
@@ -1,37 +1,75 @@
 package com.virtualdisk.util;
 
+/**
+ * Range is a conveninence class for defining where a range of locations begins and ends.
+ * Each range includes [begin, end], inclusive.
+ * 
+ * @author  Nicholas Tietz
+ */
 public class Range
 {
-
+    /**
+     * The beginning of the range.
+     */
     protected long begin;
+
+    /**
+     * The ending of the range.
+     */
     protected long end;
 
-    public Range(long b, long e)
+    /**
+     * @param   begin   the beginning of the range
+     * @param   end     the ending of the range
+     */
+    public Range(long begin, long end)
     {
-        begin = b;
-        end = e;
+        this.begin = begin;
+        this.end = end;
     }
 
+    /**
+     * Returns the beginning of a range.
+     * @return  the beginning of the range
+     */
     public long getBegin()
     {
         return begin;
     }
 
+    /**
+     * Returns the ending of a range.
+     * @return  the ending of the range
+     */
     public long getEnd()
     {
         return end;
     }
     
+    /**
+     * Checks whether the location is contained in the inclusive interval [begin, end].
+     * @param   location    the location we wish to check inclusion of
+     * @return  true the location is within [begin, end], false otherwise
+     */
     public boolean contains(long location)
     {
         return begin <= location && location <= end;
     }
     
+    /**
+     * Checks whether or not this is a validly constructed range.
+     * @return  true if the values of the range are valid, false if they are logically inconsistent
+     */
     public boolean valid()
     {
         return begin <= end;
     }
 
+    /**
+     * Checks whether a given location is immediately outside the range.
+     * @param   location    the location we wish to check adjacency of
+     * @return  true if the location is in [begin-1, end+1] but not in [begin, end], false otherwise
+     */
     public boolean adjacent(long location)
     {
         if (location+1 == begin || end+1 == location)
